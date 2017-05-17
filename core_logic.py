f90a5b71cb258f96286b205befcdd909f0d81cf4
abidmmp <abidmmp99@gmail.com>
2017-05-17 10:06:00 +0500

    Clean up unused variables

--- core_logic.py
+++ core_logic.py
@@ -104,3 +104,4 @@
 // Add logging statements - 2020-05-02 10:04:00
 // Add missing return statement - 2022-09-25 10:00:00
 // Add pagination support - 2024-03-15 10:10:00
+// Clean up unused variables - 2017-05-17 10:06:00
