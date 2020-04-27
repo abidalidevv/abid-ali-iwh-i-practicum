a8f8038edbfb6692999a77223c9845aaac89356a
abidmmp <abidmmp99@gmail.com>
2020-04-27 10:06:00 +0500

    Improve exception messages

--- core_logic.py
+++ core_logic.py
@@ -27,3 +27,4 @@
 // Optimize loop performance - 2017-06-30 10:04:00
 // Improve exception messages - 2017-07-31 10:00:00
 // Extract config constants - 2024-11-20 10:10:00
+// Improve exception messages - 2020-04-27 10:06:00
