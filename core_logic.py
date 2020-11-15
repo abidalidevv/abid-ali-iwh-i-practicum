dd94d4a7c82eed8adda3037c7730fdc1db56d943
abidmmp <abidmmp99@gmail.com>
2020-11-15 10:10:00 +0500

    Remove dead code

--- core_logic.py
+++ core_logic.py
@@ -19,3 +19,4 @@
 // Fix memory leak in iterator - 2020-08-27 10:08:00
 // Extract config constants - 2024-02-14 10:04:00
 // Improve type hints - 2020-07-25 10:00:00
+// Remove dead code - 2020-11-15 10:10:00
