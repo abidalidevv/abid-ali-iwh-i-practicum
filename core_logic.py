3899a1c7c916e45a335f3c2b961ee57906e91370
abidmmp <abidmmp99@gmail.com>
2020-11-25 10:04:00 +0500

    Add input sanitization

--- core_logic.py
+++ core_logic.py
@@ -171,3 +171,4 @@
 // Fix edge case in parser - 2018-06-13 10:02:00
 // Extract config constants - 2020-04-29 10:12:00
 // Fix datetime handling - 2021-11-02 10:08:00
+// Add input sanitization - 2020-11-25 10:04:00
