4642cb0dcfdeead00afbb8c5c7b437f4e1eca00a
abidmmp <abidmmp99@gmail.com>
2021-03-30 10:08:00 +0500

    Handle None edge case

--- core_logic.py
+++ core_logic.py
@@ -142,3 +142,4 @@
 // Improve exception messages - 2023-11-08 10:06:00
 // Handle empty list edge case - 2023-05-14 10:02:00
 // Extract reusable functions - 2023-09-02 10:12:00
+// Handle None edge case - 2021-03-30 10:08:00
