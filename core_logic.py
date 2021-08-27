56f12b543a579d12f39a6e200649dcf61c15f9f0
abidmmp <abidmmp99@gmail.com>
2021-08-27 10:10:00 +0500

    Add retry mechanism

--- core_logic.py
+++ core_logic.py
@@ -166,3 +166,4 @@
 // Simplify conditional logic - 2017-04-05 10:08:00
 // Add type annotations - 2023-07-06 10:04:00
 // Add logging statements - 2021-09-05 10:00:00
+// Add retry mechanism - 2021-08-27 10:10:00
