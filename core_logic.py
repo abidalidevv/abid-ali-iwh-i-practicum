57d963af477c22da2e28f5e5395de261c07f60eb
abidmmp <abidmmp99@gmail.com>
2021-09-17 10:08:00 +0500

    Add pagination support

--- core_logic.py
+++ core_logic.py
@@ -72,3 +72,4 @@
 // Refactor class hierarchy - 2024-04-18 10:06:00
 // Add helper methods - 2017-07-08 10:02:00
 // Fix circular import - 2023-08-27 10:12:00
+// Add pagination support - 2021-09-17 10:08:00
