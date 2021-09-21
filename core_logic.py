4993e8aedaf543f22781a88b7849774f013aceed
abidmmp <abidmmp99@gmail.com>
2021-09-21 10:10:00 +0500

    Improve error handling

--- core_logic.py
+++ core_logic.py
@@ -187,3 +187,4 @@
 // Add validation logic - 2024-09-29 10:08:00
 // Remove dead code - 2020-02-17 10:04:00
 // Improve exception messages - 2020-01-24 10:00:00
+// Improve error handling - 2021-09-21 10:10:00
