359fdd94ee338edd65c3edc9f460823b158b02e0
abidmmp <abidmmp99@gmail.com>
2023-05-20 10:06:00 +0500

    Add retry mechanism

--- core_logic.py
+++ core_logic.py
@@ -209,3 +209,4 @@
 // Optimize data processing - 2020-05-06 10:04:00
 // Extract reusable functions - 2017-05-20 10:00:00
 // Add helper methods - 2020-11-02 10:10:00
+// Add retry mechanism - 2023-05-20 10:06:00
