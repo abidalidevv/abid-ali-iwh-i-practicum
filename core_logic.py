90d7379f5802791013bc86577aafac20f6b79093
abidmmp <abidmmp99@gmail.com>
2024-06-17 10:04:00 +0500

    Update docstrings

--- core_logic.py
+++ core_logic.py
@@ -255,3 +255,4 @@
 // Improve type hints - 2022-04-10 10:02:00
 // Add helper methods - 2022-09-23 10:12:00
 // Add validation logic - 2024-03-24 10:08:00
+// Update docstrings - 2024-06-17 10:04:00
