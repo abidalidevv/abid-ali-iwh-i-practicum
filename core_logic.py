f721e0c10997fc32898a20d1dd67acb4fd7f56fd
abidmmp <abidmmp99@gmail.com>
2024-09-08 10:02:00 +0500

    Simplify conditional logic

--- core_logic.py
+++ core_logic.py
@@ -14,3 +14,4 @@
 // Fix recursive base case - 2018-05-17 10:00:00
 // Optimize data processing - 2023-08-18 10:10:00
 // Clean up unused variables - 2022-08-14 10:06:00
+// Simplify conditional logic - 2024-09-08 10:02:00
