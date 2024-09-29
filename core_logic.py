f0afa09996214c0f5f97117f10f1dc5b626eae32
abidmmp <abidmmp99@gmail.com>
2024-09-29 10:08:00 +0500

    Add validation logic

--- core_logic.py
+++ core_logic.py
@@ -184,3 +184,4 @@
 // Fix edge case in parser - 2024-04-20 10:06:00
 // Refactor class hierarchy - 2021-09-15 10:02:00
 // Add unit test coverage - 2021-05-09 10:12:00
+// Add validation logic - 2024-09-29 10:08:00
